8af7e4762692670212f8728026aacc6558a58ad6
dektlong <dekel.tankel@gmail.com>
2018-12-12 08:23:18 -0800

    display low numbers first

--- wordcount.sh
+++ wordcount.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tr -d ',.?!:"' | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' |sort | uniq -c | sort -n
+tr -d ',.?!:"' | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' |sort | uniq -c | sort -n -r
